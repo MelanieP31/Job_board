0f6b38c44673352221b541c89fa87b7915275200
MelanieP31 <melanie.picot01@gmail.com>
2024-10-27 19:54:10 +0100

    MAJ card and navigation

--- frontend/src/app/advertisements/advertisements.component.ts
+++ frontend/src/app/advertisements/advertisements.component.ts
@@ -10,12 +10,24 @@ import { AdvertisementsService } from '../service/advertisements.service';
 export class AdvertisementsComponent implements OnInit {
   advertisements : AdvertisementsCard[] = []
 
+  isModalOpen : number | null = null;
+
+
   constructor(private advertisementService: AdvertisementsService) {}
 
   ngOnInit(): void {
       this.advertisementService.getAdvertisements().subscribe((advertisements) =>{
         this.advertisements = advertisements;
       });
+  }
+
+  //modal
+  openModal(index: number){
+    this.isModalOpen = index;
+  }
+
+  closeModal(){
+    this.isModalOpen = null;
   }
 
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,20 +6,24 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AdvertisementsComponent } from './advertisements/advertisements.component';
+import { CommonModule } from '@angular/common';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    AdvertisementsComponent
+    AdvertisementsComponent,
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     AppRoutingModule,
     NgbModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideHttpClient(withInterceptorsFromDi())
   ],
   bootstrap: [AppComponent]
 })
