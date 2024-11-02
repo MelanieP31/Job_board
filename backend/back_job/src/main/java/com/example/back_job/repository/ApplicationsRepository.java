ab56e96fc72933b4d1f84f8d282b2912cb64be6a
MelanieP31 <melanie.picot01@gmail.com>
2024-11-02 17:43:33 +0100

    delete java for C#

--- backend/back_job/src/main/java/com/example/back_job/repository/ApplicationsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.back_job.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.back_job.entity.Applications;
-
-public interface ApplicationsRepository extends JpaRepository<Applications, Integer> {
-
-}
